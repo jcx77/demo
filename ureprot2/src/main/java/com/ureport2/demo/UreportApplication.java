940503d41e2ca46a9a94fb74388e91629df9fcb9
kaixuewocao.3ka <jcx7@qq.com>
2020-11-25 19:52:26 +0800

    add ureport

---
+++ ureprot2/src/main/java/com/ureport2/demo/UreportApplication.java
@@ -0,0 +1,13 @@
+package com.ureport2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UreportApplication.class, args);
+    }
+
+}
---
+++ ureprot2/src/main/java/com/ureport2/demo/custom/ReportConstant.java
@@ -0,0 +1,10 @@
+package com.ureport2.demo.custom;
+
+public interface ReportConstant {
+    String ROW_HIGHT = "18";
+    String COLUMN_WIDTH = "60";
+    String FONT_SIZE = "10";
+    String BORDER_WIDTH = "1";
+    String BORDER_COLOR = "0,0,0";
+
+}
